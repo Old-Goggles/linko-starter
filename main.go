6c8de6748e04ded0e6060d6f7de45318f52e425a
Nathan Conlon <nconlon@pm.me>
2026-07-17 13:31:34 -0500

    feat: switch file logging to JSON format

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func initializeLogger() (*slog.Logger, closeFunc, error) {
 
 	bufferedFile := bufio.NewWriterSize(file, 8192)
 
-	infoHandler := slog.NewTextHandler(bufferedFile, &slog.HandlerOptions{
+	infoHandler := slog.NewJSONHandler(bufferedFile, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
 	})
 
